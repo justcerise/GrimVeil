39eb28028e7ee9a35284289b68d969021ed7368c
NERITH <cerise.ov@gmail.com>
2025-08-17 18:42:48 -0400

    Initial commit

---
+++ src/main/java/net/grimveil/GrimVeilClient.java
@@ -0,0 +1,10 @@
+package net.grimveil;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class GrimVeilClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
